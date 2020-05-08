2fcad63bb7f368eec14f5e49de5b8d6a46b7ae3b
Alexandr Murashkin <alexandr.murashkin@gmail.com>
2020-05-08 11:25:38 +0300

    Update main4.js

--- lesson_6/js/main4.js
+++ lesson_6/js/main4.js
@@ -1,61 +1,3 @@
-// Вывод на экран:
- // alert ("Привет, Мир!");
-
-// переменные:
- var a = 35;
- var b = "Привет, ";
- var ab = "Мир!";
- var c = false;
- var d = 5; 
-
- // alert (a + d);
-
-// условия:
-//  if (a == 35) {
-//  	alert ("Всё верно!");
-//  }
-
- // a = 456;
- // a = prompt ("Напишите значение переменной а");
-
- // if (a == 37) {
- // 	alert ("a = 37");
-
- // } else {
- // 	alert ("Что-то другое");
- // }
-
- // // циклы:
- // for (var i = 0; i < 6; i++) {
- // 	alert (i);
- // }
- 	
-// // объекты:
-// var user = {
-// 	"name" : "Петя",
-// 	"surname" : "Васичкин",
-// 	"age" : 25
-// }
-
-// alert (user["name"]) 
-// // или:
-// alert (user.name)
-
-// массивы:
-// var mas = ["Петя", "Васичкин", 25];
-// alert (mas[1])
-
-// // функции:
-// function doMagic (a,b) {
-// 	alert (a + b);
-
-// }
-
-// doMagic (5,10);
-// doMagic (1,2);
-
-
-
 // main4.js:
 
 var btn = document.getElementById("btn");
@@ -82,7 +24,7 @@ $(function () {
 	        var imagePos = $(this).offset().top;
 
 	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+650) {
+	        if (imagePos < topOfWindow+800) {
 	            $(this).addClass("fadeInDown");
 	        }
 	    });
@@ -93,7 +35,7 @@ $(function () {
 	        var imagePos = $(this).offset().top;
 
 	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+700) {
+	        if (imagePos < topOfWindow+800) {
 	            $(this).addClass("fadeInLeft");
 	        }
 	    });
@@ -120,6 +62,5 @@ $(function () {
 	        }
 	    });
 	});
-
 
 })
